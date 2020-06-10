cc30d74c277a7208038df9ba21282f76a1ec711b
JetVayne <edward871130@gmail.com>
2020-06-11 07:27:59 +0800

    add glance

---
+++ Face-Feat-Tool-Glance/glance/gar_wizard.py
@@ -0,0 +1,13 @@
+# coding: utf-8
+from deepface import DeepFace
+from glance.jf_ult.log_tool import LogTool
+
+
+class GarWizard:
+    @staticmethod
+    def get_face_dict(img_path: str) -> dict:
+        try:
+            return DeepFace.analyze(img_path)
+        except Exception as e:
+            msg = LogTool.pp_exception(e)
+            print(msg)
